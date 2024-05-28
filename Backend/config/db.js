b56ede98a1849dbf6fa7e6eae17bce0070d16f9b
bhavyakashmira <bhavyakashmira@gmail.com>
2024-05-28 22:05:31 +0530

    changed

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-    await mongoose.connect('mongodb+srv://bhavyakashmira:vXSYrHmVWm4BKOly@cluster0.cla6kqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log("DB connected"))
+    await mongoose.connect(process.env.API_KEY).then(()=>console.log("DB connected"))
 }
 
